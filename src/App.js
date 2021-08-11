390124a6a2eee12ce6fa4458444f3236e28cfde9
Gevorg Ghazaryan <gevorg.ghazaryan@gatsoft.am>
2021-08-11 12:38:08 +0400

    chnages

--- src/App.js
+++ src/App.js
@@ -5,31 +5,59 @@ import TableCell from './TableCell';
 
 function App() {
 
-  const [data, setData] =   useState('');
-  const [info, setInfo] = useState()
+  const [data, setData] = useState('');
+  const [info, setInfo] = useState();
 
-  useEffect(() => {
-    if(data.length == 2) {
-     const interval = setTimeout(() => {
-        fetch(`https://corona-api.com/countries/${data}`)
-        .then(stream => stream.json())
-        .then(results => setInfo(results))
-      }, 1000)
+  // useEffect(() => {
+  //   if(data.length == 2) {
+  //    const interval = setTimeout(() => {
+  //       fetch(`https://corona-api.com/countries/${data}`)
+  //       .then(stream => stream.json())
+  //       .then(results => setInfo(results))
+  //     }, 1000)
 
-      return () => {
-        clearTimeout(interval)
-      }
+  //     return () => {
+  //       clearTimeout(interval)
+  //     }
+  //   }
+  // }, [data])
+
+  // const inputChangeHandler = (evt) =>{
+  //     // setData(evt.target.value);
+  //     if (evt.target.value > 2) {
+  //        fetch(`https://corona-api.com/countries/${data}`)
+  //         .then(stream => stream.json())
+  //         .then(results => setInfo(results))
+  //     }
+  // }
+
+  function DefaultFetch(value) {
+    fetch(`https://corona-api.com/countries/${value ? value : 'us'}`)
+         .then(stream => stream.json())
+         .then(results => {
+           setInfo(results)
+          //  setData('')
+          } )
+  }
+  function handleChange(evt) {
+    if(evt.target.value.length === 2) {
+      DefaultFetch(evt.target.value);
+
     }
-  }, [data])
+    setData(evt.target.value);
+  }
+  
+  useEffect(() => {
+    DefaultFetch()
+  }, [])
 
   return (
     <div className="App">
       <input 
       type="text"
+      value={data}
       placeholder="Enter Valid character.."
-      onChange={(evt) => {
-        setData(evt.target.value);
-      }}
+      onChange={handleChange}
       ></input>
       <TableCell info={info} />
     </div>
